63cf796b8df7a25a7a939b80b35b3a3cd2bf0c46
jose garcia <ing.manu@gmail.com>
2023-11-03 13:20:22 -0500

    implementacion total de plantas por tipo

--- src/app/planta/planta.component.ts
+++ src/app/planta/planta.component.ts
@@ -5,17 +5,25 @@ import { PlantaService } from './planta.service';
 @Component({
   selector: 'app-planta',
   templateUrl: './planta.component.html',
-  styleUrls: ['./planta.component.css']
+  styleUrls: ['./planta.component.css'],
 })
 export class PlantaComponent implements OnInit {
-
   public plantas: Array<Planta> = [];
-  
-  constructor(private plantaService: PlantaService) { }
+  public plantasInterior: number = 0;
+  public plantasExterior: number = 0;
+
+  constructor(private plantaService: PlantaService) {}
 
   getPlantas() {
-    this.plantaService.getPlantas().subscribe(plantas => {
+    this.plantaService.getPlantas().subscribe((plantas) => {
       this.plantas = plantas;
+      this.plantas.forEach((planta) => {
+        if (planta.tipo == 'Interior') {
+          this.plantasInterior++;
+        } else {
+          this.plantasExterior++;
+        }
+      });
     });
   }
 
